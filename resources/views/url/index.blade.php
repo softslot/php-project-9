0ca9839492716031f16aea9a18f7a0d41763e0bb
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-19 19:21:39 +0300

    update views

--- resources/views/url/index.blade.php
+++ resources/views/url/index.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    Список ссылок
+    <div class="container-lg">
+        <h1 class="mt-5 mb-3">Сайты</h1>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover text-nowrap">
+                <tr>
+                    <th>ID</th>
+                    <th>Имя</th>
+                    <th>Последняя проверка</th>
+                    <th>Код ответа</th>
+                </tr>
+                @foreach($urls as $url)
+                    <tr>
+                        <td>{{ $url->id }}</td>
+                        <td><a href="{{ route('urls.show', $url->id) }}">{{ $url->name }}</a></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            </table>
+
+            {{ $urls->links('vendor.pagination.bootstrap-5') }}
+        </div>
+    </div>
 @endsection
