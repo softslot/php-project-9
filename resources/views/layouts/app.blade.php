2cfafc0533bfc92519bbd7a364015f452150f5ac
Konstantin Korolenko <softslot-com@yandex.ru>
2022-09-04 15:43:33 +0300

    add vite in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    {{--  Bootstrap  --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    @vite(['resources/sass/app.scss'])
 
     <title>Анализатор страниц</title>
 </head>
