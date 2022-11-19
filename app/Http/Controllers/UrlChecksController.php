a188520049019cf8179b3b317fe61589a48ad806
Konstantin Korolenko <softslot-com@yandex.ru>
2022-11-19 18:56:03 +0300

    add parsing seo data

--- app/Http/Controllers/UrlChecksController.php
+++ app/Http/Controllers/UrlChecksController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use DiDom\Document;
+use DiDom\Query;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -15,12 +17,29 @@ class UrlChecksController
                 abort('404');
             });
 
-        $response = Http::get($url->name);
+        try {
+            $response = Http::get($url->name);
+        } catch (\Exception) {
+            flash('Произошла ошибка при проверке, не удалось подключиться')->error();
+
+            return redirect()->route('urls.show', $url->id);
+        }
+
+        $document = new Document($url->name, true);
+        $h1 = optional($document->first('h1'))->text();
+        $title = optional($document->first('title'))->text();
+        $description = optional(
+            $document->first("//meta[contains(@name, 'description')]",
+                Query::TYPE_XPATH)
+        )->getAttribute('content');
 
         DB::table('url_checks')
             ->insert([
                 'url_id' => $url->id,
                 'status_code' => $response->status(),
+                'h1' => $h1,
+                'title' => $title,
+                'description' => $description,
                 'created_at' => now()->toDateTimeString(),
             ]);
 
