6c37f47114a7796c6ec4ed2c8a438d55c7371a95
Konstantin Korolenko <softslot-com@yandex.ru>
2022-11-22 18:55:48 +0300

    update home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
     <div class="container-lg mt-3">
         <div class="row">
             <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
-                <h1 class="display-3">Анализатор страниц@@@</h1>
+                <h1 class="display-3">Анализатор страниц</h1>
                 <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
                 <form action="{{ route('urls.index') }}" method="post" class="d-flex justify-content-center">
                     @csrf
