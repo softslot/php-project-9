e085d396806aeea69daba4ccb25cf1933d82bf49
Konstantin Korolenko <softslot-com@yandex.ru>
2022-11-24 15:52:28 +0300

    lint fix

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@ Route::controller(UrlsController::class)
     ->name('urls.')
     ->group(function () {
         Route::get('urls', 'index')->name('index');
-        Route::post('urls','store')->name('store');
-        Route::get('urls/{id}','show')->name('show');
+        Route::post('urls', 'store')->name('store');
+        Route::get('urls/{id}', 'show')->name('show');
     });
 
 Route::controller(UrlChecksController::class)
     ->name('url_checks.')
     ->group(function () {
-        Route::post('urls/{id}/checks','store')->name('store');
+        Route::post('urls/{id}/checks', 'store')->name('store');
     });
