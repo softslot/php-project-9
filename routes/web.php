f53c15ba3d0888b54375f7a477ce2c413cc60a2c
Konstantin Korolenko <softslot-com@yandex.ru>
2022-12-04 17:12:06 +0300

    refactoring routes/web

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\UrlChecksController;
 use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index')->name('home');
+Route::view('/', 'index')
+    ->name('home');
 
 Route::resource('urls', UrlController::class)
     ->only(['index', 'store', 'show']);
