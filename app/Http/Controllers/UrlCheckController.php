19e5ae512eea2e0ae7ee8967de1aeab43087e4f7
Konstantin Korolenko <softslot-com@yandex.ru>
2023-01-19 18:37:02 +0300

    fix error 500

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -17,6 +17,24 @@ class UrlCheckController
 
         try {
             $response = Http::get($url->name);
+            $document = new Document($response->body());
+            
+            DB::table('url_checks')
+                ->insert([
+                    'url_id' => $url->id,
+                    'status_code' => $response->status(),
+                    'h1' => optional($document->first('h1'))->text(),
+                    'title' => optional($document->first('title'))->text(),
+                    'description' => optional($document->first('meta[name=description]'))
+                        ->getAttribute('content'),
+                    'created_at' => now()->toDateTimeString(),
+                ]);
+
+            if ($response->serverError()) {
+                flash('Проверка была выполнена успешно, но сервер ответил с ошибкой')->warning();
+            } else {
+                flash('Страница успешно проверена')->success();
+            }
         } catch (\Exception $exception) {
             $errorMessage = $exception->getMessage();
             flash($errorMessage)->error();
@@ -24,24 +42,6 @@ class UrlCheckController
             return redirect()
                 ->route('urls.show', $url->id)
                 ->withErrors($errorMessage);
-        }
-
-        $document = new Document($response->body());
-        DB::table('url_checks')
-            ->insert([
-                'url_id' => $url->id,
-                'status_code' => $response->status(),
-                'h1' => optional($document->first('h1'))->text(),
-                'title' => optional($document->first('title'))->text(),
-                'description' => optional($document->first('meta[name=description]'))
-                    ->getAttribute('content'),
-                'created_at' => now()->toDateTimeString(),
-            ]);
-
-        if ($response->serverError()) {
-            flash('Проверка была выполнена успешно, но сервер ответил с ошибкой')->warning();
-        } else {
-            flash('Страница успешно проверена')->success();
         }
 
         return redirect()->route('urls.show', $url->id);
