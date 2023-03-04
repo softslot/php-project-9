60451520a911eba8a5b6f655cb7fb9b0b68c164b
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-05 00:42:02 +0300

    test exception

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use DiDom\Document;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -19,7 +20,7 @@ class UrlCheckController
 
         try {
             $response = Http::get($url->name);
-        } catch (\GuzzleHttp\Exception\RequestException $exception) {
+        } catch (ConnectionException $exception) {
             $errorMessage = $exception->getMessage();
             flash($errorMessage)->error();
 
